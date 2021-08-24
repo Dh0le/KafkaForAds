fa11ea1b89045f182a9ffe6536c794181437a59f
Dh0le <i@zjiaqi.com>
2021-08-23 23:08:26 -0500

    Create method in consumer that Async commit and sync commit.

--- src/main/java/com/imooc/kafkastudy/MyConsumer.java
+++ src/main/java/com/imooc/kafkastudy/MyConsumer.java
@@ -1,5 +1,6 @@
 package com.imooc.kafkastudy;
 
+import org.apache.kafka.clients.consumer.CommitFailedException;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -34,17 +35,81 @@ public class MyConsumer {
                             record.partition(),
                             record.key(),
                             record.value()));
-                    if(record.value().equals("done")){
+                    if(record.value().equals("done")) {
                         flag = false;
                     }
-                    if(!flag)break;
                 }
+                if(!flag)break;
             }
 
         }finally {
             consumer.close();
         }
 
+    }
+
+    // Sync commit will block the thread, but it will return error if commit went wrong.
+    private static void generalConsumeMessageSyncCommit(){
+        properties.put("auto.commit.offset",false);
+        consumer = new KafkaConsumer<String, String>(properties);
+        consumer.subscribe(Collections.singleton("imooc-kafka-study"));
+        try{
+            while(true){
+                boolean flag = true;
+                ConsumerRecords<String,String> records = consumer.poll(100);
+                for(ConsumerRecord<String,String> record:records){
+                    System.out.println(String.format("topic:%s,partition:%s,key:%s,value:%s",
+                            record.topic(),
+                            record.partition(),
+                            record.key(),
+                            record.value()));
+                    if(record.value().equals("done")) {
+                        flag = false;
+                    }
+                }
+                try{
+                    consumer.commitSync();
+                }catch (CommitFailedException ex){
+                    ex.printStackTrace();
+                }
+                if(!flag)break;
+            }
+
+        }finally {
+            consumer.close();;
+        }
+    }
+
+    // much faster than Sync commit but we know nothing about the result.
+    private static void generalConsumeMessageAsyncCommit(){
+        properties.put("auto.commit.offset",false);
+        consumer = new KafkaConsumer<String, String>(properties);
+        consumer.subscribe(Collections.singleton("imooc-kafka-study"));
+        try{
+            while(true){
+                boolean flag = true;
+                ConsumerRecords<String,String> records = consumer.poll(100);
+                for(ConsumerRecord<String,String> record:records){
+                    System.out.println(String.format("topic:%s,partition:%s,key:%s,value:%s",
+                            record.topic(),
+                            record.partition(),
+                            record.key(),
+                            record.value()));
+                    if(record.value().equals("done")) {
+                        flag = false;
+                    }
+                }
+                try{
+                    consumer.commitAsync();
+                }catch (CommitFailedException ex){
+                    ex.printStackTrace();
+                }
+                if(!flag)break;
+            }
+
+        }finally {
+            consumer.close();;
+        }
     }
 
     public static void main(String[] args) {
